d22196f39e5d8ab83c9f2e4c19b406c145609602
Erik Kristian Sverre Uri <erik.uri@bellevuecollege.edu>
2018-02-21 13:27:37 -0800

    two pipes

--- pipe.py
+++ pipe.py
@@ -11,17 +11,19 @@ class Pipe:
                       [self.width + 360, randint(80, 320)]]
         self.sprites = [ pygame.image.load('assets/sprites/pipe-red.png').convert_alpha(),
                          pygame.image.load('assets/sprites/pipe-green.png').convert_alpha() ]
-        self.sprite = self.sprites[int]
+        self.bottom = self.sprites[int]
+        self.top = pygame.transform.flip(self.bottom,0,1)
 
         
 
-    def paint(self, DISPLAY): # hitching
+    def paint(self, DISPLAY): # hitching, rewrite entire class later
         if self.pipes[0][0] <= -52:
             self.pipes.append([self.width, randint(80, 320)]) #gap is because of
             self.pipes.pop(0)
 
         for pipe in self.pipes:
-            DISPLAY.blit(self.sprite, (pipe[0], pipe[1]))
+            DISPLAY.blit(self.bottom, (pipe[0], pipe[1]))
+            DISPLAY.blit(self.top, (pipe[0], pipe[1] - 420))
             pipe[0] -= 1
         
 
