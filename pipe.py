ab834849b15d72986d2ac0473396ca5d112d0a15
Erik Kristian Sverre Uri <erik.uri@bellevuecollege.edu>
2018-02-21 18:09:28 -0800

    pipes with fixed gap

--- pipe.py
+++ pipe.py
@@ -4,17 +4,16 @@ class Pipe(pygame.sprite.Sprite):
     
     def __init__(self, x, y, color, direction):
         pygame.sprite.Sprite.__init__(self)
-        self.x = x
         self.images = [ pygame.image.load('assets/sprites/pipe-red.png').convert_alpha(),
                         pygame.image.load('assets/sprites/pipe-green.png').convert_alpha() ]
         self.image = self.images[color]
         if direction == 1:
-            self.image = pygame.transform.flip(self,0,1)
+            self.image = pygame.transform.flip(self.image,0,1)
             self.rect = self.image.get_rect()
-            self.rect.center = (200, 0)
+            self.rect.bottomleft = (x, y - 100)
         else:
             self.rect = self.image.get_rect()
-            self.rect.center = (200, 255)
+            self.rect.topleft = (x, y)
         
     def update(self): # hitching, rewrite entire class later
         self.rect.move_ip(-1, 0)
--- pipes.py
+++ pipes.py
@@ -11,11 +11,15 @@ class Pipes(pygame.sprite.Group):
         self.density = density
         self.counter = self.width / self.density
         self.color = color
-        self.add(Pipe(0,0,color,0))
+        self.gap = randint(180,320)
+        self.add(Pipe(self.width, self.gap, self.color, 0))
+        self.add(Pipe(self.width, self.gap, self.color, 1))
 
     def update(self):
         self.counter -= 1
         if self.counter < 0:
-            self.add(Pipe(0,0, self.color,0))
+            self.gap = randint(180,320)
+            self.add(Pipe(self.width, self.gap, self.color, 0))
+            self.add(Pipe(self.width, self.gap, self.color, 1))
             self.counter = self.width / self.density
         pygame.sprite.Group.update(self)
